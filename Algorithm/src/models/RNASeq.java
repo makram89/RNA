0f5bbe350469921decce36391f17243c8cb472a2
makram89 <34419130+makram89@users.noreply.github.com>
2020-06-18 16:41:15 +0200

    init java

---
+++ Algorithm/src/models/RNASeq.java
@@ -0,0 +1,16 @@
+package models;
+
+import javax.naming.Name;
+import java.util.ArrayList;
+
+public class RNASeq {
+
+    private String name;
+    private String sequence;
+    private String initialDotBracket;
+    public ArrayList<RNASingleChain> singleChains;
+
+    RNASeq(String name , String input){
+
+    }
+}
